18b6d98cd24878199fd3c54afbde8afb71ccf696
arina-p <118122134+arina-petruseva@users.noreply.github.com>
2023-04-30 12:42:55 -0400

    add prodigy

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let temp = Math.floor(Math.random()*30);
 let weather = ['sunny','rainy','windy','thunderstorm','overcast'];
-let music = ['Pink Floyd','Iron Maiden','Lady Gaga'];
+let music = ['Pink Floyd','Iron Maiden','Lady Gaga','Prodigy'];
 let yourMood
 if (temp === 1 || temp === 21) {
     yourMood = `It's ${temp} degree outside and it's ${weather[Math.floor(Math.random()*5)]}. I should listen to ${music[Math.floor(Math.random()*3)]}.`
