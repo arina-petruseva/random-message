cf8abd482214dddcc414c95910d10b74d6e501b2
arina-petruseva <118122134+arina-petruseva@users.noreply.github.com>
2025-03-18 19:39:44 -0400

    fix spelling

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let weather = ['sunny','rainy','windy','thunderstorm','overcast'];
 let music = ['Pink Floyd','Iron Maiden','Lady Gaga','Prodigy','Joss Stone'];
 let yourMood;
 if (temp === 1 || temp === 21) {
-    yourMood = `It's ${temp} degree outside and it's ${weather[Math.floor(Math.random()*5)]}. I should listen to ${music[Math.floor(Math.random()*5)]}.`
+    yourMood = `It's ${temp} degree outside and it's ${weather[Math.floor(Math.random()*5)]}. I should listen ${music[Math.floor(Math.random()*5)]}.`
 } else {
-    yourMood = `It's ${temp} degrees outside and it's ${weather[Math.floor(Math.random()*5)]}. I should listen to ${music[Math.floor(Math.random()*5)]}.`
+    yourMood = `It's ${temp} degrees outside and it's ${weather[Math.floor(Math.random()*5)]}. I should listen ${music[Math.floor(Math.random()*5)]}.`
 }
 console.log(yourMood)
